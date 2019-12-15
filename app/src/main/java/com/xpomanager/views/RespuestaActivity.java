2b463b1718d33a19bab60d1377b356c8d5dceed9
Marcelo Goncevatt <mgoncevatt.cep@gmail.com>
2019-12-15 20:34:51 +0100

    Actualizacion RespuestaActivity

--- app/src/main/java/com/xpomanager/views/RespuestaActivity.java
+++ app/src/main/java/com/xpomanager/views/RespuestaActivity.java
@@ -8,11 +8,15 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +37,8 @@ public class RespuestaActivity extends AppCompatActivity {
     private final static String APP_FOLDER = Environment.getExternalStorageDirectory().getAbsolutePath() + "/XPOmanager/data/";
     private final static String IMAGES_FOLDER = APP_FOLDER + "Imagenes/Elements/";
     private final static String QSTIMG_FOLDER = APP_FOLDER + "Imagenes/Preguntas/";
+
+    public static CountDownTimer mcd;
 
     /*************
      * ATRIBUTOS *
@@ -75,11 +81,19 @@ public class RespuestaActivity extends AppCompatActivity {
 
         // - - - - - - - - - - Imagen de Personaje
         ConstraintLayout constraintLayoutBackground = findViewById(R.id.ConstraintLayoutBackground);
-        String filename;
-        if (preguntaIdioma.getLinkRespuestaCorrecta()!=null) { filename = QSTIMG_FOLDER + preguntaIdioma.getLinkRespuestaCorrecta(); }
-        else { filename = IMAGES_FOLDER + "appimage.jpg"; }
-        // lee archivo grafico y retorna un bitmap
-        if (new File( filename ).exists()) { bitmap = BitmapFactory.decodeFile( filename ); }
+        String filename = preguntaIdioma.getLinkRespuestaCorrecta();
+        if ( (filename != null) && (new File( QSTIMG_FOLDER + filename ).exists()) )
+        {
+            bitmap = BitmapFactory.decodeFile( QSTIMG_FOLDER + filename );
+        }
+        else if ( new File( IMAGES_FOLDER + "appimage.jpg" ).exists() )
+        {
+            bitmap = BitmapFactory.decodeFile(IMAGES_FOLDER + "appimage.jpg" );
+        }
+        else
+        {
+            bitmap = BitmapFactory.decodeFile(IMAGES_FOLDER + "defaultbg.jpg");
+        }
         constraintLayoutBackground.setBackground( new BitmapDrawable(getResources(), bitmap));
 
         // - - - - - - - - - - Imagen de Personaje
@@ -97,7 +111,8 @@ public class RespuestaActivity extends AppCompatActivity {
         // se calcula el color de fondo en base a posicion y longitud de arralist de niveles
         // PE, si es de 3 sera de 0=#00FF00 1=#FFFF00 2=@FF0000
         TextView textViewDificultad = findViewById(R.id.TextViewDificultad);
-        textViewDificultad.setText( controladorJuego.getNivel().getNombre() );
+        //textViewDificultad.setText( controladorJuego.getNivel().getNombre() );
+        textViewDificultad.setText( controladorJuego.getNivel().getTraducciones().get( controladorJuego.getIdioma() ) );
         textViewDificultad.setBackgroundColor( Color.rgb(50, 130, 50) );
 
         // - - - - - - - - - - Imagen de Inicio
@@ -120,11 +135,11 @@ public class RespuestaActivity extends AppCompatActivity {
 
         // - - - - - - - - - - Tezxt e Imagen de QR URL LINK
         TextView textViewPregunta = findViewById(R.id.TextViewPregunta);
-        ImageView imageViewQR = findViewById(R.id.ImageViewQR);
+        ImageView ImageViewQR = findViewById(R.id.ImageViewQR);
         if ( preguntaIdioma.getStringLinkRespuestaCorrecta() != null) {
             textViewPregunta.setText( preguntaIdioma.getStringLinkRespuestaCorrecta() );
             bitmap = QR.encodeAsBitmap(preguntaIdioma.getStringLinkRespuestaCorrecta());
-            imageViewPersonaje.setImageBitmap(bitmap);
+            ImageViewQR.setImageBitmap(bitmap);
         }
 
         // - - - - - - - - - - Resumen preguntas y aciertos
@@ -160,23 +175,48 @@ public class RespuestaActivity extends AppCompatActivity {
         // - - - - - - - - - - Next Button
         Button buttonSiguiente = findViewById(R.id.ButtonSiguiente);
         buttonSiguiente.setText( literals.getNextButton() ); // set NEXT button Text
+        Animation anim = new AlphaAnimation(0.4f, 1.0f);
+        anim.setDuration(1000); //You can manage the blinking time with this parameter
+        anim.setStartOffset(100);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        buttonSiguiente.startAnimation(anim);
         // - - - - - - - - - - Accion de boton NEXT
-        buttonSiguiente.setOnClickListener(new View.OnClickListener() {
+        buttonSiguiente.setOnClickListener( new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                if (controladorJuego.hasNextPregunta()) {
-                    Intent intent = new Intent(view.getContext(), PreguntaActivity.class);
-                    startActivity(intent);
-                }
-                else
-                {
-                    Intent intent = new Intent(view.getContext(), ResumenActivity.class);
-                    startActivity(intent);
-                }
-                finish();
-            }
-        });
+            public void onClick(View view) { theExecActivity(); }
+        } );
+
+        // - - - - - - - - - - Jump on TimeOut
+        theCountDown();
 
     }
 
+    // metodo de ejecucion de activity segun estado de pregunta
+    public void theExecActivity()
+    {
+        mcd.cancel();
+        Intent intent;
+        if ( controladorJuego.hasNextPregunta() ) { intent = new Intent( getBaseContext(), PreguntaActivity.class); }
+        else { intent = new Intent( getBaseContext(), ResumenActivity.class ); }
+        startActivity( intent );
+        finish();
+    }
+
+    // metodo de activacion de contador por segundos y ejecucion de llamada a otro activity
+    public void theCountDown()
+    {
+        mcd = new CountDownTimer(15000, 1000)
+        {
+            public void onTick(long millisUntilFinished)
+            {
+                /*mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);*/
+            }
+            public void onFinish()
+            {
+                try { theExecActivity(); } catch (Exception ex) {  }
+            }
+        }.start();
+    }
 }
